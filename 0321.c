598dcfc901edc53556a4d5afc8a3fa0fa632bf3e
test <123456@qq.com>
2024-03-27 14:53:42 +0800

    linux版

--- 0321.c
+++ 0321.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-#include <curses.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <termios.h>
 
 #define WIDTH 20
 #define HEIGHT 20
@@ -9,51 +11,39 @@ typedef struct {
     int y;
 } Point;
 
-int snakeLength = 1;
-Point snake[WIDTH * HEIGHT];
-Point food;
-char direction = 'd'; 
-void updateSnake() {
-    int i;
-    // 移动蛇身
-    for (i = snakeLength - 1; i > 0; i--) {
-    	snake[i] = snake[i - 1];
-     }              
-    // 根据当前方向移动蛇头
-    switch (direction) {
-	case 'w': snake[0].y--; break;
-	case 's': snake[0].y++; break;
-	case 'a': snake[0].x--; break;
-	case 'd': snake[0].x++; break;
-    }
-}
-
-void draw() {
-    int i;
-    // 绘制蛇身
-    for (i = 0; i < snakeLength; i++) {
-        printf("%c", (i == 0) ? '*' : '+'); // 头部用 * 表示，身体用 + 表示
-    }
-}
-
-void gameLoop(){
-    while(2){
-	updateSnake();
-	system("cls");
-	draw();
-	sleep(100);
-	if(_kbhit()){
-	    direction = _getch();
-	}
-    }
-}
-
 int main() {
-    // 初始化蛇和食物位置
-    snake[0].x = WIDTH / 2;
-    snake[0].y = HEIGHT / 2;
-    food.x = rand() % WIDTH;
-    food.y = rand() % HEIGHT;
-    gameLoop();
+    Point snake = {WIDTH / 2, HEIGHT / 2};
+    char direction = 'd'; // 初始方向向右
+
+    while (1) {
+        system("clear"); // 清空屏幕
+	// 绘制蛇
+	printf("*");
+
+	// 接收用户输入
+	struct termios oldattr, newattr;
+        tcgetattr(STDIN_FILENO, &oldattr);
+        newattr = oldattr;
+        newattr.c_lflag &= ~(ICANON | ECHO); // 禁用标准模式和回显
+        tcsetattr(STDIN_FILENO, TCSANOW, &newattr);
+
+        char input = getchar();
+        if (input == 'w' || input == 'a' || input == 's' || input == 'd') {
+            direction = input;
+        }
+
+        tcsetattr(STDIN_FILENO, TCSANOW, &oldattr); // 恢复控制台设置
+
+	// 更新蛇的位置
+	switch (direction) {
+            case 'w': snake.y--; break;
+            case 'a': snake.x--; break;
+            case 's': snake.y++; break;
+            case 'd': snake.x++; break;
+        }
+
+        // 暂停一会儿
+        usleep(100000); // 100 毫秒
+    }
     return 0;
 }
